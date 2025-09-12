c837abcac2fc52e171ab2bf88e8117fa1ac8aa73
Jean-Christian Denis <contact@jcdenis.fr>
2025-09-12 18:37:19 +0200

    fix template

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -1,16 +1,19 @@
 <?php
-/**
- * @package Dotclear
- *
- * @copyright Olivier Meunier & Association Dotclear
- * @copyright GPL-2.0-only
- */
+// Language: Français
+// Module: CategoriesPage - 1.3.3
+// Date: 2025-04-28 16:45:27
+// Author: , contact@jcdenis.fr
+// Translated with Translater - 2025.03.03
+
+
 #
 # DOT NOT MODIFY THIS FILE !
 #
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Categories Page']                       = 'Page des catégories';
-L10n::$locales['Link to categories']                    = 'Lien vers la page des catégories';
+L10n::$locales['Last entry:'] = 'Dernier billet :';
+L10n::$locales['Last comment:'] = 'Dernier commentaire :';
+L10n::$locales['Categories Page'] = 'Page des catégories';
+L10n::$locales['Link to categories'] = 'Lien vers la page des catégories';
 L10n::$locales['Add a public page for categories list'] = 'Ajoute une page publique listant les catégories';
